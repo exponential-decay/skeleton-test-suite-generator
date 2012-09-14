68e26ca38883f9442e095fbdbae71ebf2192726c
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2012-09-14 23:28:18 +0100

    [ignore] Ironing out version number.

--- skeletongenerator.py
+++ skeletongenerator.py
@@ -27,7 +27,7 @@ def readPronomExport(config):
 
 def parseCommandLine():
 	parser = argparse.ArgumentParser(description='Tool for the automated generation of digital objects based on the digital signatures documented in the PRONOM database maintained by The National Archives, UK.')
-	parser.add_argument('--version', help="Display the version number of the tool", action='version', version='%(prog)s 0.1 BETA')
+	parser.add_argument('--version', help="Display the version number of the tool", action='version', version='%(prog)s v0.1-BETA')
 	args = parser.parse_args()
 
 def main():
