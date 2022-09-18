b0126e06cff824c1d5a09fb829e770ff0093d332
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2022-09-18 12:59:44 +0200

    Further Pylint fixes

--- deletefiles.py
+++ deletefiles.py
@@ -1,9 +1,11 @@
+"""Cleans up after the skeleton suite generator."""
+
 import configparser as ConfigParser
 import shutil
 
 
 def cleanup():
-
+    """Cleanup function for the skeleton suite generator."""
     config = ConfigParser.RawConfigParser()
     config.read("skeletonsuite.cfg")
     shutil.rmtree(config.get("locations", "output"), True)
